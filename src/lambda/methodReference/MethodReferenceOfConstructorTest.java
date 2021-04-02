69738f0d5a7a8de538287a755bdf78b93f2b9d1c
soosue <yunsoo0510@daum.net>
2021-04-02 09:42:40 +0900

    lambda식 메소드참조(생성자) 써보기 : 배열 생성

--- src/lambda/methodReference/MethodReferenceOfConstructorTest.java
+++ src/lambda/methodReference/MethodReferenceOfConstructorTest.java
@@ -4,17 +4,25 @@ import java.util.function.*;
 
 public class MethodReferenceOfConstructorTest {
     public static void main(String[] args) {
-        Supplier<ConstructorTest> s = ConstructorTest::new;
-        Function<String, ConstructorTest> f = ConstructorTest::new;
-        BiFunction<String, String, ConstructorTest> bf = ConstructorTest::new;
+        Supplier<ConstructorTest> s = () -> new ConstructorTest();
+        Supplier<ConstructorTest> s2 = ConstructorTest::new;
+        Function<String, ConstructorTest> f = (ss) -> new ConstructorTest(ss);
+        Function<String, ConstructorTest> f2 = ConstructorTest::new;
+        BiFunction<String, String, ConstructorTest> bf = (ss1, ss2) -> new ConstructorTest(ss1, ss2);
+        BiFunction<String, String, ConstructorTest> bf2 = ConstructorTest::new;
 
-        ConstructorTest ct = s.get();
-        ConstructorTest ct2 = f.apply("s");
-        ConstructorTest ct3 = bf.apply("s", "s2");
+        Function<Integer, int[]> ar = x -> new int[x];
+        Function<Integer, int[]> ar2 = int[]::new;
+        int[] intArr = ar2.apply(5);
+
+        ConstructorTest ct = s2.get();
+        ConstructorTest ct2 = f2.apply("s");
+        ConstructorTest ct3 = bf2.apply("s", "s2");
 
         System.out.println("ct = " + ct);
         System.out.println("ct2 = " + ct2);
         System.out.println("ct3 = " + ct3);
+        System.out.println("intArr size = " + intArr.length);
 
     }
 }
